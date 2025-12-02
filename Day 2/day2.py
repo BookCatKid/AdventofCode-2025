fbc54406e85714087ece51a27ffb7ccc6ec78bf3
BookCatKid <99609593+BookCatKid@users.noreply.github.com>
2025-12-02 11:36:00 -0800

    refactor(day2): remove duplication

--- Day 2/day2.py
+++ Day 2/day2.py
@@ -6,36 +6,31 @@ with open("day2input.txt") as f:
 total_invalid_count_1 = 0
 total_invalid_count_2 = 0
 
-def check_number_1(num):
-    if str(num)[0] == "0":
+def check_number(num, pattern):
+    num_str = str(num)
+    if num_str[0] == "0":
         return False
-    return bool(re.fullmatch(r"(\d+)\1", str(num)))
+    return bool(re.fullmatch(pattern, num_str))
 
-def check_number_2(num):
-    if str(num)[0] == "0":
-        return False
-    return bool(re.fullmatch(r"(.+)\1+", str(num)))
-
-def check_range_1(range_str):
+def sum_invalid_numbers(range_str, checker):
     total_return = 0
     start, end = range_str.split("-")
     for num in range(int(start), int(end) + 1):
-        if check_number_1(num):
+        if checker(num):
             total_return += num
     return total_return
 
-def check_range_2(range_str):
-    total_return = 0
-    start, end = range_str.split("-")
-    for num in range(int(start), int(end) + 1):
-        if check_number_2(num):
-            total_return += num
-    return total_return
+
+def is_invalid_part1(num):
+    return check_number(num, r"(\d+)\1")
+
+
+def is_invalid_part2(num):
+    return check_number(num, r"(.+)\1+")
 
 for range_str in ranges:
-    total_invalid_count_1 += check_range_1(range_str)
-    total_invalid_count_2 += check_range_2(range_str)
-    # print(f"Invalid numbers sum for range {range_str}: {check_range(range_str)}")
+    total_invalid_count_1 += sum_invalid_numbers(range_str, is_invalid_part1)
+    total_invalid_count_2 += sum_invalid_numbers(range_str, is_invalid_part2)
 
 print("Total number of invalid numbers (Part 1):", total_invalid_count_1)
 print("Total number of invalid numbers (Part 2):", total_invalid_count_2)
